47fb3a2b12cb9decd904aeafd1d401275b1a8645
ash <ash.s.fischer@gmail.com>
2019-05-29 14:02:05 -0500

    graphic update

---
+++ Assets/Scripts/MissileSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileSpawner : MonoBehaviour
+{
+    public GameObject missiles;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnMissiles());
+    }
+
+    IEnumerator SpawnMissiles()
+    {
+        yield return null;
+    }
+}
